786ddd044fc9e8648868d6e52cdc47eb47797dc6
Érick Firmo <erickfirmo1996@gmail.com>
2021-12-15 14:19:57 -0300

    Create submitActions function in customers listing

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -19,37 +19,55 @@
             </tbody>
         </table>
     </div>
-
 @push('js')
 
 <script type="text/javascript">
-  $(function () {
-    
-    var table = $('.yajra-datatable').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: "{{ route('users.list') }}",
-        columns: [
-            {data: 'id', name: 'id'},
-            {data: 'name', name: 'name'},
-            {data: 'email', name: 'email'},
-            {data: 'date', name: 'date'},
-            {
-                data: 'action', 
-                name: 'action', 
-                orderable: true, 
-                searchable: true
+// 
+function submitAction(a) {
+    let accepted_methods = ['POST', 'DELETE', 'PUT', 'PATCH'];
+    let method = $(a).data('method');
+    let url = $(a).data('url');
+
+    if (accepted_methods.includes(method))
+    {
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: {
+                _token: '{{ csrf_token() }}',
+                _method: method
             },
-        ],
-        "language": {
-            "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese.json"
-        }
-    });
-    
-  });
+            success: function(data)
+            {
+                table.row( $(this).parents('tr')).remove().draw();
+                alert(data);
+            }
+        });
+    }
+}
+
+//
+var table = $('.yajra-datatable').DataTable({
+    processing: true,
+    serverSide: true,
+    ajax: "{{ route('users.list') }}",
+    columns: [
+        {data: 'id', name: 'id'},
+        {data: 'name', name: 'name'},
+        {data: 'email', name: 'email'},
+        {data: 'date', name: 'date'},
+        {
+            data: 'action', 
+            name: 'action',
+            orderable: true, 
+            searchable: true
+        },
+        
+    ],
+    "language": {
+        "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese.json"
+    }
+});
 </script>
-
-
 @endpush
-
 @endsection
