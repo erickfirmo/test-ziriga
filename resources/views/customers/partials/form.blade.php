0bb51a60cd2a104789f5c48c1eb44788457181cb
Érick Firmo <erickfirmo1996@gmail.com>
2021-12-16 03:34:15 -0300

    Update customers form

--- resources/views/customers/partials/form.blade.php
+++ resources/views/customers/partials/form.blade.php
@@ -2,36 +2,37 @@
     @csrf
     <div class="form-group mb-3 col-4">
         {!! Form::label('name', 'Nome', ['for' => 'name']) !!}
-        {!! Form::text('name', $customer->name ?? null,['class' => 'form-control', 'id' => 'name', 'required' => 'required']) !!}
+        {!! Form::text('name', $customer->name ?? null,['class' => 'form-control custom-validator', 'id' => 'name', 'required' => 'required']) !!}
         <div id="name-error" class="col-form-label form-txt-danger d-none"></div>
     </div>
 
     <div class="form-group mb-3 col-4">
         {!! Form::label('email', 'E-mail', ['for' => 'email']) !!}
-        {!! Form::email('email', $customer->email ?? null, ['class' => 'form-control', 'id' => 'email', 'required' => 'required']) !!}
+        {!! Form::email('email', $customer->email ?? null, ['class' => 'form-control custom-validator', 'id' => 'email', 'required' => 'required']) !!}
         <div id="email-error" class="col-form-label form-txt-danger d-none"></div>
     </div>
 
     <div class="form-group mb-3 col-4">
         {!! Form::label('dob', 'Data de Nascimento', ['for' => 'dob']) !!}
-        {!! Form::date('dob', $customer->dob ?? null, ['class' => 'form-control', 'id' => 'dob']) !!}
+        {!! Form::date('dob', $customer->dob ?? null, ['class' => 'form-control custom-validator', 'id' => 'dob']) !!}
         <div id="dob-error" class="col-form-label form-txt-danger d-none"></div>
     </div>
 
     <div class="form-group mb-3 col-6">
         {!! Form::label('password', 'Senha', ['for' => 'password']) !!}
-        {!! Form::password('password', ['class' => 'form-control', 'id' => 'password', 'required' => 'required']) !!}
+        {!! Form::password('password', ['class' => 'form-control custom-validator', 'id' => 'password', 'required' => 'required']) !!}
         <div id="password-error" class="col-form-label form-txt-danger d-none"></div>
     </div>
 
     <div class="form-group mb-3 col-6">
         {!! Form::label('password_confirmation', 'Confirmar Senha', ['for' => 'password_confirmation']) !!}
-        {!! Form::password('password_confirmation', ['class' => 'form-control', 'id' => 'password_confirmation', 'required' => 'required']) !!}
+        {!! Form::password('password_confirmation', ['class' => 'form-control custom-validator', 'id' => 'password_confirmation', 'required' => 'required']) !!}
         <div id="password_confirmation-error" class="col-form-label form-txt-danger d-none"></div>
     </div>
 </div>
 
-<a href="{{ route('users.index') }}" type="button" class="btn btn-lg btn-light">Cancelar</a>
-<button type="submit" class="btn btn-lg btn-success">{{ !isset($customer) ? 'Salvar' : 'Atualizar' }}</button>
+<button type="submit" class="btn btn-primary">{{ !isset($customer) ? 'Salvar' : 'Atualizar' }}</button>
+<a href="{{ route('users.index') }}" type="button" class="btn btn-light">Cancelar</a>
+
 
 {!! Form::close() !!}
