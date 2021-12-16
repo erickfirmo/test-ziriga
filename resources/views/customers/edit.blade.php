e977b883e2202293b250eb2f8c53591852a9777a
Érick Firmo <erickfirmo1996@gmail.com>
2021-12-16 03:33:51 -0300

    Update edit customers view

--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -2,22 +2,73 @@
 
 @section('content')
 
-{!! Form::model($customer, ['route' => ['users.update', $customer->id]]) !!}
+{!! Form::model($customer, ['route' => ['users.update', $customer->id], 'method' => 'PUT']) !!}
 
 @include('customers.partials.form')
 
 @push('js')
-    @if(session()->exists("message"))
-    <script>
-        $(function() {
-            Swal.fire(
-                'Sucesso!',
-                '{{ session()->get("success") }}',
-                'success'
-            )
-        });
-    </script>
-    @endif
-@endpush
 
+<script>
+    $('form').submit(function(e){
+        e.preventDefault();
+
+        let form = $(this);
+        let url = $(this).attr('action');
+        var formData = {
+            _token: "{{ csrf_token() }}",
+            name: $("#name").val(),
+            email: $("#email").val(),
+            date: $("#dob").val(),
+            password: $("#password").val(),
+            password_confirmation: $("#password_confirmation").val(),
+        };
+
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: formData,
+            //contentType: "application/json",
+            success: function(data)
+            {
+                Swal.fire(
+                    'Sucesso!',
+                    'Usuário atualizado com sucesso!',
+                    'success'
+                )
+            },
+            error: function (data) {
+                // verify if has validation errors
+                if (data.status == 422) {
+                    let errors = JSON.parse(data.responseText);
+
+                    for (let field in errors) {
+                        $('#'+field+'-error').text(errors[field]);
+                        $('#'+field).addClass('border-danger');
+                        $('#'+field+'-error').removeClass('d-none');
+                    }
+                    
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Algo deu errado!',
+                    });
+                }
+            },
+        });
+    })
+</script>
+
+@if(session()->exists("message"))
+<script>
+    $(function() {
+        Swal.fire(
+            'Sucesso!',
+            '{{ session()->get("success") }}',
+            'success'
+        )
+    });
+</script>
+@endif
+@endpush
 @endsection
