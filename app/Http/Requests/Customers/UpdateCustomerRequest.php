ad42b7ae5f60b36571037095adcef33db89836f5
Érick Firmo <erickfirmo1996@gmail.com>
2021-12-16 02:00:49 -0300

    Create UpdateCustomerRequest

---
+++ app/Http/Requests/Customers/UpdateCustomerRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Customers;
+
+class UpdateCustomerRequest extends CustomerRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:255',
+            'email' => 'required|max:255|unique:customers,email,'.$this->route('id'),
+            'dob' => 'nullable|date',
+            //'password' => 'min:6|required_with:password_confirmation|same:password_confirmation',
+            'password' => 'required|confirmed|min:6',
+            'password_confirmation' => 'required|min:6',
+        ];
+    }
+}
